64babf3ca2d06dbeb08f65b0772f6dae535041a1
zooshme <contact@zoosh.me>
2016-06-16 14:31:11 +0100

    latest updates

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,40 +1,48 @@
-function init() {
-  var App = document.getElementById('app');
-  var conversationList = document.createElement('ol')
-  conversationList.className = "conversation-list";
-  App.innerHTML = conversationList;
-
-  hz('conversations').limit(10000).watch().subscribe(function(items) {
-    var conversationItems = items.map(function(item) {
-      var conversationItem = document.createElement('li');
-      conversationItem.className = 'conversation-item';
-      conversationItem.innerHTML =
-      return '<li class="conversation-item">' + item.title + '<a href="#" data-id="' + item.id + '" class="delete-button">Delete</a></li>';
-    });
-    conversationList.innerHTML(conversationItems);
-  });
-
-  var $Form = $('#form');
-
-  $Form.on('submit', function(e) {
+var hz = new Horizon({
+  authType: 'twitter'
+});
+if (!hz.hasAuthToken()) {
+  $('#forbidden').show();
+  $('#app').hide();
+  $('#sign-in').on('click', function(e) {
     e.preventDefault();
-    hz('conversations').store({
-      title: $('#title').val(),
-      description: $('#description').val()
+    hz.authEndpoint('twitter').subscribe(function(endpoint) {
+      window.location.pathname = endpoint;
+    })
+    return false;
+  })
+} else {
+  hz.onReady(function() {
+    $('#forbidden').hide();
+    $('#app').show();
+
+    var user;
+
+    hz.currentUser().fetch().subscribe(function(user) {
+      if (typeof user.nickname !== 'undefined') {
+        $('#nickname-form').hide();
+
+      } else {
+        $('#nickname-form').show();
+        $('#nickname-form').on('submit', function(e) {
+          e.preventDefault();
+          hz('users').upsert({
+            id: user.id,
+            nickname: $('#nickname').val()
+          })
+          return false;
+        })
+      }
     });
-    return false;
+    $('#form').on('submit', function(e) {
+      e.preventDefault();
+      hz('messages').store({
+        message: $('#message').val(),
+        user_id: user.id
+      })
+      return false;
+    })
   });
 
-  $DeleteButtons = $('.delete-button');
-
-  $DeleteButtons.on('click', function(e) {
-    e.preventDefault();
-    hz('conversations').remove($(this).data('id'));
-    return false;
-  });
-
-}
-
-function isUserLoggedIn() {
-
+  hz.connect();
 }
